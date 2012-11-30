0cc25d3156992eabb71a5022613e6384c0319328
Elad Meidar <elad@eizesus.com>
2012-11-30 23:40:54 +0200

    [TEST] All green

--- spec/happy_api_configuration_spec.rb
+++ spec/happy_api_configuration_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe HappyApi::Configuration do
-  it "should not include port in base url if port is 80" do
-    User.api_end_point.should eql("http://localhost")
+
+  describe "#api_port" do
+    it "should not include port in base url if port is 80" do
+      User.api_end_point.should eql("http://localhost")
+    end
+
+    it "should include port in api base url if port != 80" do
+      User.api_port = 777
+      User.api_end_point.should eql("http://localhost:777")
+    end
   end
 
-  it "should include port in api base url if port != 80" do
-    User.api_port = 777
-    User.api_end_point.should eql("http://localhost:777")
+  describe "#api_primary_key" do
+
+    it "should default to :id" do
+      User.api_primary_key.should eql(:id)
+    end
   end
 
   describe "#configure" do
